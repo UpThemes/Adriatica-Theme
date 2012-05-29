4656068202d43fac2c8717b8c32aed32414af6d5
Chris Wallace <chris@walmedia.com>
2012-05-29 10:27:52 -0500

    Fixed sidebar: referenced incorrect sidebar.

--- sidebar.php
+++ sidebar.php
@@ -1,10 +1,10 @@
 <div id="sidebar">
   <?php if( is_active_sidebar('sidebar') ): ?>
   <ul>
-    <?php dynamic_sidebar(); ?>
- </ul>
-	<?php else : ?>
-   <?php echo the_widget("WP_Widget_Pages"); ?>
+    <?php dynamic_sidebar('sidebar'); ?>
+  </ul>
+  <?php else : ?>
+  <?php echo the_widget("WP_Widget_Pages"); ?>
   <?php endif; ?>
 
 </div><!-- /#sidebar -->
